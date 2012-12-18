55cd564a3c4f62b0ccf4a48e35585ac6b1ada162
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-19 00:12:22 +0900

    example changed

--- example/growthforecast.rb
+++ example/growthforecast.rb
@@ -21,3 +21,19 @@ class GrowthForecast
     end
   end
 end
+
+g = GrowthForecast::Client.new("127.0.0.1", 5125)
+
+config = {
+  :service  => "apache",
+  :section  => "scoreboard",
+  :gragh    => "woreker",
+}
+
+data = {
+  :number   => 7,
+  :color    => "#333399",
+  :mode     => "count",
+}
+
+p g.post(config, data)
