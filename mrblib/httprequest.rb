499ccf6c30cf80906f1413b2735d6486c3c2359c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-31 16:12:47 +0900

    support escape mehtod for original mruby

--- mrblib/httprequest.rb
+++ mrblib/httprequest.rb
@@ -36,7 +36,7 @@ class HttpRequest
   def encode_parameters(params, delimiter = '&', quote = nil)
     if params.is_a?(Hash)
       params = params.map do |key, value|
-        sprintf("%s=%s%s%s", key, quote, value, quote)
+        sprintf("%s=%s%s%s", escape(key), quote, escape(value), quote)
       end
     else
       params = params.map { |value| escape(value) }
@@ -56,5 +56,26 @@ class HttpRequest
       request["Content-Length"] = (request["body"] || '').length
     end
     request
+  end
+  #def escape(str, unsafe = nil)
+  def escape(str)
+    reserved_str = [
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+      "-", ".", "_", "~"
+    ]
+    tmp = ''
+    str = str.to_s
+    str.size.times do |idx|
+      chr = str[idx]
+      if reserved_str.include?(chr)
+        tmp += chr
+      else
+        tmp += "%" + chr.unpack("H*").first.upcase
+      end
+    end
+    #puts "#{str}: #{tmp}"
+    tmp
   end
 end
